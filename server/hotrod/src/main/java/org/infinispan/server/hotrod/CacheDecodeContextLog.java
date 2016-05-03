e70cbfb60a3d16596d6f1fa05b2167766d796bb4
William Burns <mudokonman@gmail.com>
2016-05-03 14:50:34 -0400

    ISPN-6580 Hotrod performance regressions after ISPN-5342 ISPN-6545
    * Make sure logger is not created per CacheDecodeContext instance

---
+++ server/hotrod/src/main/java/org/infinispan/server/hotrod/CacheDecodeContextLog.java
@@ -0,0 +1,14 @@
+package org.infinispan.server.hotrod;
+
+import org.infinispan.server.core.logging.JavaLog;
+import org.infinispan.util.logging.LogFactory;
+
+/**
+ * // TODO: Document this
+ *
+ * @author wburns
+ * @since 4.0
+ */
+public class CacheDecodeContextLog {
+   static final JavaLog log = LogFactory.getLog(CacheDecodeContext.class, JavaLog.class);
+}
