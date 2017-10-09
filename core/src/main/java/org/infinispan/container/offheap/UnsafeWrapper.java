5c95706da1457a2e660d53664fd6f80b470a2797
William Burns <mudokonman@gmail.com>
2017-10-09 03:39:48 -0400

    ISPN-8380 OffHeap doesn't expire entries on access
    * Cleanup trace arguments
    * Add array string messages

--- core/src/main/java/org/infinispan/container/offheap/UnsafeWrapper.java
+++ core/src/main/java/org/infinispan/container/offheap/UnsafeWrapper.java
@@ -1,5 +1,6 @@
 package org.infinispan.container.offheap;
 
+import org.infinispan.commons.util.Util;
 import org.infinispan.util.logging.Log;
 import org.infinispan.util.logging.LogFactory;
 
@@ -20,44 +21,53 @@ public class UnsafeWrapper {
 
    private UnsafeWrapper() { }
 
-   public void putLong(long var1, long var3) {
+   public void putLong(long address, long value) {
       if (trace) {
-         log.tracef("Wrote long value %d to address %d", var3, var1);
+         log.tracef("Wrote long value %d to address %d", value, address);
       }
-      UNSAFE.putLong(var1, var3);
+      UNSAFE.putLong(address, value);
    }
 
-   public void putInt(long var1, int var3) {
+   public void putInt(long address, int value) {
       if (trace) {
-         log.tracef("Wrote int value %d to address %d", var3, var1);
+         log.tracef("Wrote int value %d to address %d", value, address);
       }
-      UNSAFE.putInt(var1, var3);
+      UNSAFE.putInt(address, value);
    }
 
-   public long getLong(long var1) {
-      long var3 = UNSAFE.getLong(var1);
+   public long getLong(long address) {
+      long value = UNSAFE.getLong(address);
       if (trace) {
-         log.tracef("Retrieved long value %d from address %d", var3, var1);
+         log.tracef("Retrieved long value %d from address %d", value, address);
       }
-      return var3;
+      return value;
    }
 
-   public int getInt(long var1) {
-      int var3 = UNSAFE.getInt(var1);
+   public int getInt(long address) {
+      int value = UNSAFE.getInt(address);
       if (trace) {
-         log.tracef("Retrieved int value %d from address %d", var3, var1);
+         log.tracef("Retrieved int value %d from address %d", value, address);
       }
-      return var3;
+      return value;
    }
 
-   public int arrayBaseOffset(Class<?> var1) {
-      return UNSAFE.arrayBaseOffset(var1);
+   public int arrayBaseOffset(Class<?> arrayClass) {
+      return UNSAFE.arrayBaseOffset(arrayClass);
    }
 
-   public void copyMemory(Object var1, long var2, Object var4, long var5, long var7) {
+   public void copyMemory(Object srcBase, long srcOffset, Object destBase, long destOffset, long bytes) {
       if (trace) {
-         log.tracef("Copying memory of object %s offset by %d to %s offset by %d with a total of %d bytes", var1, var2, var4, var5, var7);
+         log.tracef("Copying memory of object %s offset by %d to %s offset by %d with a total of %d bytes",
+               printable(srcBase), srcOffset, printable(destBase), destOffset, bytes);
       }
-      UNSAFE.copyMemory(var1, var2, var4, var5, var7);
+      UNSAFE.copyMemory(srcBase, srcOffset, destBase, destOffset, bytes);
+   }
+
+   static Object printable(Object obj) {
+      if (obj instanceof byte[]) {
+         return Util.printArray((byte[]) obj);
+      }
+
+      return obj;
    }
 }
